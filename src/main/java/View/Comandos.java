e93f37b1b8f9fd0cc172b285ced87376043a7928
unknown <jonasarome21@gmail.com>
2023-02-24 00:30:39 -0300

    Version 0.0.1a Creados los botones dentro de la ventana Comandos

--- src/main/java/View/Comandos.java
+++ src/main/java/View/Comandos.java
@@ -5,6 +5,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
 
 public class Comandos extends JFrame {
 
@@ -36,6 +39,37 @@ public class Comandos extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 436, 263);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblComandos = new JLabel("COMANDOS");
+		lblComandos.setHorizontalAlignment(SwingConstants.CENTER);
+		lblComandos.setBounds(133, 31, 170, 30);
+		panel.add(lblComandos);
+		
+		JButton btnHigiene = new JButton("Higiene");
+		btnHigiene.setBounds(133, 72, 170, 30);
+		panel.add(btnHigiene);
+		
+		JButton btnAlimentacion = new JButton("Alimentación");
+		btnAlimentacion.setBounds(133, 113, 170, 30);
+		panel.add(btnAlimentacion);
+		
+		JButton btnAcciones = new JButton("Acciones");
+		btnAcciones.setBounds(133, 153, 170, 30);
+		panel.add(btnAcciones);
+		
+		JButton btnRespuestas = new JButton("Respuestas");
+		btnRespuestas.setBounds(133, 194, 170, 30);
+		panel.add(btnRespuestas);
+		
+		JButton btnVolver = new JButton("Volver");
+		btnVolver.setBounds(337, 229, 89, 23);
+		panel.add(btnVolver);
 	}
 
 }
