de423cb47872ea3a098c3430008619ef11a720a2
unknown <jonasarome21@gmail.com>
2023-02-24 00:04:58 -0300

    Version 0.0.1a Creada la ventana de emergencia con los respectivos botones

--- src/main/java/View/Emergencias.java
+++ src/main/java/View/Emergencias.java
@@ -5,6 +5,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class Emergencias extends JFrame {
 
@@ -31,11 +36,45 @@ public class Emergencias extends JFrame {
 	 */
 	public Emergencias() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 414, 228);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 400, 191);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JButton btnAvisoRobo = new JButton("Aviso de Robo");
+		btnAvisoRobo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnAvisoRobo.setBounds(115, 93, 176, 30);
+		panel.add(btnAvisoRobo);
+		
+		JButton btnAvisoAccidente = new JButton("Aviso de accidente");
+		btnAvisoAccidente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnAvisoAccidente.setBounds(115, 134, 176, 30);
+		panel.add(btnAvisoAccidente);
+		
+		JLabel lblEmergencias = new JLabel("Emergencias");
+		lblEmergencias.setHorizontalAlignment(SwingConstants.CENTER);
+		lblEmergencias.setBounds(115, 11, 176, 30);
+		panel.add(lblEmergencias);
+		
+		JButton btnAvisoConflicto = new JButton("Aviso de Conflicto Domestico");
+		btnAvisoConflicto.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnAvisoConflicto.setBounds(115, 52, 176, 30);
+		panel.add(btnAvisoConflicto);
 	}
-
 }
--- src/main/java/View/Main.java
+++ src/main/java/View/Main.java
@@ -5,6 +5,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Main extends JFrame {
 
@@ -31,11 +34,48 @@ public class Main extends JFrame {
 	 */
 	public Main() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 288);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 48, 416, 192);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JButton btnConfiguracion = new JButton("Configuración");
+		btnConfiguracion.setBounds(135, 50, 140, 30);
+		panel.add(btnConfiguracion);
+		
+		JButton btnEmergencias = new JButton("Emergencias");
+		btnEmergencias.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnEmergencias.setBounds(135, 90, 140, 30);
+		panel.add(btnEmergencias);
+		
+		JButton btnComandos = new JButton("Comandos");
+		btnComandos.setBounds(135, 10, 140, 30);
+		panel.add(btnComandos);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(0, 0, 436, 49);
+		contentPane.add(panel_1);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(0, 48, 10, 203);
+		contentPane.add(panel_2);
+		
+		JPanel panel_2_1 = new JPanel();
+		panel_2_1.setBounds(426, 48, 10, 203);
+		contentPane.add(panel_2_1);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBounds(10, 241, 416, 10);
+		contentPane.add(panel_3);
 	}
-
 }
